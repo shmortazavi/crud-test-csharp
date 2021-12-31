5fb34418d3078b37686a7d7531b26165a47c2ba2
shmortazavi <mortazavidev@gmail.com>
2022-01-01 02:32:50 +0330

    adding gerenic read and write repository interfaces

---
+++ Mc2.CrudTest.Domain/SeedWorks/IReadRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Domain.SeedWorks
+{
+    public interface IReadRepository<T> where T : Entity
+    {
+        Task<T> GetById(Guid id);
+        Task<List<T>> GetAll();
+        Task<IQueryable<T>> Find(Expression<Func<T, bool>> expression);
+    }
+}
---
+++ Mc2.CrudTest.Domain/SeedWorks/IWriteRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Domain.SeedWorks
+{
+    public interface IWriteRepository<T> where T : Entity
+    {
+        Task<T> Insert(T entity);
+        Task Update(T entity);
+        Task<bool> Delete(T entity);
+    }
+}
