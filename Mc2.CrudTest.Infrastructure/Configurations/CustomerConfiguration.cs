b27bf3c57a739468e74dc5d9bc318d44c739c7d8
shmortazavi <mortazavidev@gmail.com>
2022-01-01 02:44:01 +0330

    adding Mc2DbContext/ adding Customer to Mc2DbContext/ creating and adding CustomerConfiguration to Mc2DbContext

---
+++ Mc2.CrudTest.Infrastructure/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Mc2.CrudTest.Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mc2.CrudTest.Infrastructure.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.FirstName).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.LastName).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.PhoneNumber).HasMaxLength(20).IsRequired();
+            builder.Property(p => p.Email).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.DateOfBirth).IsRequired();
+        }
+    }
+}
---
+++ Mc2.CrudTest.Infrastructure/Mc2DbContext.cs
@@ -0,0 +1,20 @@
+﻿using Mc2.CrudTest.Domain.Customers;
+using Mc2.CrudTest.Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mc2.CrudTest.Infrastructure
+{
+    public class Mc2DbContext : DbContext
+    {
+        public Mc2DbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+        }
+    }
+}
