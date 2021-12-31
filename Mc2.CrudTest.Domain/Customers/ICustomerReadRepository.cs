4be096eac0e857d37a17bfc5f8c05621ca0c5b22
shmortazavi <mortazavidev@gmail.com>
2022-01-01 02:34:27 +0330

    adding customer's read and write repository interfaces

---
+++ Mc2.CrudTest.Domain/Customers/ICustomerReadRepository.cs
@@ -0,0 +1,8 @@
+﻿using Mc2.CrudTest.Domain.SeedWorks;
+
+namespace Mc2.CrudTest.Domain.Customers
+{
+    public interface ICustomerReadRepository : IReadRepository<Customer>
+    {
+    }
+}
---
+++ Mc2.CrudTest.Domain/Customers/ICustomerWriteRepository.cs
@@ -0,0 +1,8 @@
+﻿using Mc2.CrudTest.Domain.SeedWorks;
+
+namespace Mc2.CrudTest.Domain.Customers
+{
+    public interface ICustomerWriteRepository : IWriteRepository<Customer>
+    {
+    }
+}
