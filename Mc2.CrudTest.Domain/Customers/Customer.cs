fa9d0146cf4fd9e5e5ce97ccc9bf08996c4c1e25
shmortazavi <mortazavidev@gmail.com>
2022-01-02 00:06:40 +0330

    add bankAccountNumber to customer model

--- Mc2.CrudTest.Domain/Customers/Customer.cs
+++ Mc2.CrudTest.Domain/Customers/Customer.cs
@@ -11,44 +11,53 @@ namespace Mc2.CrudTest.Domain.Customers
         public DateTime DateOfBirth { get; private set; }
         public string PhoneNumber { get; private set; }
         public string Email { get; private set; }
+        public string BankAccountNumber { get; private set; }
         #endregion
 
         #region Ctor
-        private Customer(string firstName, string lastName, DateTime dateOfBirth, string phoneNumber, string email)
+        private Customer(string firstName, string lastName, DateTime dateOfBirth,
+                         string phoneNumber, string email, string bankAccountNumber)
         {
-            CheckInveriants(firstName, lastName, dateOfBirth, phoneNumber, email);
+            CheckInveriants(firstName, lastName, dateOfBirth, phoneNumber, email, bankAccountNumber);
 
             FirstName = firstName;
             LastName = lastName;
             DateOfBirth = dateOfBirth;
-
+            PhoneNumber = phoneNumber;
+            Email = email;
+            BankAccountNumber = bankAccountNumber;
         }
         #endregion
 
-
         #region Methods
-        public static Customer Create(string firstName, string lastName, DateTime dateOfBirth, string phoneNumber, string email)
+        public static Customer Create(string firstName, string lastName, DateTime dateOfBirth,
+                                      string phoneNumber, string email, string bankAccountNumber)
         {
-            return new Customer(firstName, lastName, dateOfBirth, phoneNumber, email)
+            return new Customer(firstName, lastName, dateOfBirth, phoneNumber, email, bankAccountNumber)
             {
                 Id = Guid.NewGuid(),
                 CreatedDate = DateTime.Now
             };
         }
 
-
-        public static Customer Update(Customer customer, string firstName, string lastName, DateTime dateOfBirth, string phoneNumber, string email)
+        public static Customer Update(Customer customer, string firstName, string lastName,
+                                      DateTime dateOfBirth, string phoneNumber, string email,
+                                      string bankAccountNumber)
         {
+            CheckInveriants(firstName, lastName, dateOfBirth, phoneNumber, email, bankAccountNumber);
+
             customer.FirstName = firstName;
             customer.LastName = lastName;
             customer.DateOfBirth = dateOfBirth;
             customer.PhoneNumber = phoneNumber;
             customer.Email = email;
             customer.UpdatedDate = DateTime.Now;
+            customer.BankAccountNumber = bankAccountNumber;
+
             return customer;
         }
 
-        private static void CheckInveriants(string firstName, string lastName, DateTime dateOfBirth, string phoneNumber, string email)
+        private static void CheckInveriants(string firstName, string lastName, DateTime dateOfBirth, string phoneNumber, string email, string bankAccountNumber)
         {
             if (string.IsNullOrEmpty(firstName))
                 throw new Exception("firstName is required!");
@@ -64,6 +73,9 @@ namespace Mc2.CrudTest.Domain.Customers
 
             if (string.IsNullOrEmpty(email))
                 throw new Exception("email is required!");
+
+            if (string.IsNullOrEmpty(bankAccountNumber))
+                throw new Exception("bankAccountNumber is required!");
         }
         #endregion
     }
