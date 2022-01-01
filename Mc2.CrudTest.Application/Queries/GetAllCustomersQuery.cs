427b6edb6c75eda2edeb8040abb9b29e0d188e4d
shmortazavi <mortazavidev@gmail.com>
2022-01-01 17:41:01 +0330

    adding cqrs Qurey for cutomers : GetAll and GetById

---
+++ Mc2.CrudTest.Application/Queries/GetAllCustomersQuery.cs
@@ -0,0 +1,48 @@
+﻿using Mc2.CrudTest.Application.Dtos;
+using Mc2.CrudTest.Domain.SeedWorks;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Application.Queries
+{
+    public record GetAllCustomersQuery : IRequest<List<CustomerResponseDto>>;
+
+    public class GetAllCustomerHandler : IRequestHandler<GetAllCustomersQuery, List<CustomerResponseDto>>
+    {
+        #region Fields
+        private readonly IReadUnitOfWork _unitOfWork;
+        #endregion
+
+        #region Ctor
+        public GetAllCustomerHandler(IReadUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        #endregion
+
+        #region Handle Method
+        public Task<List<CustomerResponseDto>> Handle(GetAllCustomersQuery request, CancellationToken cancellationToken)
+        {
+            var result = _unitOfWork.CustomerReadRepository.GetAll().Result;
+            if (result == null || result.Count <= 0)
+                throw new Exception("Any customer not found!");
+
+            var response = result.Select(x => new CustomerResponseDto
+            {
+                Id = x.Id,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                DateOfBirth = x.DateOfBirth,
+                Email = x.Email,
+                PhoneNumber = x.PhoneNumber
+            }).ToList();
+
+            return Task.FromResult(response);
+        }
+        #endregion
+    }
+}
---
+++ Mc2.CrudTest.Application/Queries/GetCustomerByIdQuery.cs
@@ -0,0 +1,51 @@
+﻿using Mc2.CrudTest.Application.Dtos;
+using Mc2.CrudTest.Domain.SeedWorks;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Application.Queries
+{
+    public record GetCustomerByIdQuery(Guid Id) : IRequest<CustomerResponseDto>;
+
+    public class GetCustomerByIdHandler : IRequestHandler<GetCustomerByIdQuery, CustomerResponseDto>
+    {
+        #region Fields
+        private readonly IReadUnitOfWork _unitOfWork;
+        #endregion
+
+        #region Ctor
+        public GetAllCustomerHandler(IReadUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        #endregion
+
+        #region Handle Method
+        public Task<CustomerResponseDto> Handle(GetCustomerByIdQuery request, CancellationToken cancellationToken)
+        {
+            var customerId = request.Id;
+            if (customerId == Guid.Empty)
+                throw new Exception("Customer Id is not correct!");
+
+            var dbCustomer = _unitOfWork.CustomerReadRepository.GetById(customerId).Result;
+            if (dbCustomer == null)
+                throw new Exception("customer not found!");
+
+            return Task.FromResult(
+                new CustomerResponseDto
+                {
+                    Id = dbCustomer.Id,
+                    FirstName = dbCustomer.FirstName,
+                    LastName = dbCustomer.LastName,
+                    DateOfBirth = dbCustomer.DateOfBirth,
+                    Email = dbCustomer.Email,
+                    PhoneNumber = dbCustomer.PhoneNumber
+                });
+        }
+        #endregion
+    }
+}
