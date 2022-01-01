5dabe561411e1ee73c08f2c92316f804ed14d42a
shmortazavi <mortazavidev@gmail.com>
2022-01-02 00:35:52 +0330

    adding new migration

--- Mc2.CrudTest.Infrastructure/Migrations/20220101205123_migration_2th.Designer.cs
+++ Mc2.CrudTest.Infrastructure/Migrations/20220101205123_migration_2th.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Mc2.CrudTest.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Mc2.CrudTest.Infrastructure.Migrations
 {
     [DbContext(typeof(Mc2DbContext))]
-    partial class Mc2DbContextModelSnapshot : ModelSnapshot
+    [Migration("20220101205123_migration_2th")]
+    partial class migration_2th
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Mc2.CrudTest.Infrastructure/Migrations/20220101205123_migration_2th.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mc2.CrudTest.Infrastructure.Migrations
+{
+    public partial class migration_2th : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BankAccountNumber",
+                table: "Customers",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BankAccountNumber",
+                table: "Customers");
+        }
+    }
+}
